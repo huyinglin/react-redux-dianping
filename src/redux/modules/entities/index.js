986113506750b8f69a857530d4a183e3a4df2771
法正 <hwj938@foxmail.com>
2019-08-18 23:33:24 +0800

    feat: udpate base

---
+++ src/redux/modules/entities/index.js
@@ -0,0 +1,12 @@
+import { combineReducers } from 'reudx';
+import products from './products';
+import shops from './shops';
+import orders from './orders';
+import comments from './comments';
+
+const rootReducer = combineReducers({
+  products,
+  shops,
+  orders,
+  comments,
+});
