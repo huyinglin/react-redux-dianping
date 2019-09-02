75fa6e42bf50599cb1983704520afb23871c72b4
法正 <hwj938@foxmail.com>
2019-09-02 21:54:21 +0800

    feat: 超值特惠组件开发结束‘

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import Category from './components/Category';
+import Headline from './components/Headline'
+import Discount from './components/Discount'
 
 class Home extends Component {
     render() {
         return (
             <div>
                 <Category/>
+                <Headline/>
+                <Discount/>
             </div>
         );
     }
