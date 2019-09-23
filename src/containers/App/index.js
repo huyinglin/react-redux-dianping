e8b26c2fb178465f3bd4e3abcd8eb887f9c84e8e
法正 <hwj938@foxmail.com>
2019-09-23 22:52:50 +0800

    feat: 实现react的lazy方法

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -4,14 +4,16 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { bindActionCreators } from 'redux';
 import ErrorToast from '../../components/ErrorToast';
 import { actions as appActions, getError } from "../../redux/modules/app";
-import Home from '../Home';
-import ProductDetail from '../ProductDetail';
-import Search from "../Search";
-import SearchResult from "../SearchResult";
-import Login from "../Login";
-import User from "../User";
 import PrivateRoute from '../PrivateRoute';
-import Purchase from "../Purchase"
+import AsyncComponent from "../../utils/AsyncComponent";
+
+const Home = AsyncComponent(() => import("../Home"));
+const ProductDetail = AsyncComponent(() => import("../ProductDetail"));
+const Search = AsyncComponent(() => import("../Search"));
+const SearchResult = AsyncComponent(() => import("../SearchResult"));
+const Login = AsyncComponent(() => import("../Login"));
+const User = AsyncComponent(() => import("../User"));
+const Purchase = AsyncComponent(() => import("../Purchase"));
 
 function App({
   error,
